fc62b1fb371bc1f1ba8ccd35c7713e5326d2f418
Arindam Pal <arindampal2000@gmail.com>
2024-10-05 22:46:25 +0530

    fd-vite-rewrite: assets, structure, Navbar

---
+++ fd-vite-rewrite/src/components/Navbar.tsx
@@ -0,0 +1,93 @@
+import "./Navbar.css";
+
+import ashvamedhaLogo from "../assets/demoPhotos/ashvamedhaLogo.png";
+
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+
+type MenuData = Array<{ title: string; path: string; className: string }>;
+
+function Navbar() {
+    const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
+
+    const menuData: MenuData = [
+        {
+            title: "Home",
+            path: "/",
+            className: "nav-text",
+        },
+        {
+            title: "About Us",
+            path: "/#aboutus",
+            className: "nav-text",
+        },
+        {
+            title: "Events",
+            path: "/events",
+            className: "nav-text",
+        },
+        {
+            title: "Team",
+            path: "/team",
+            className: "nav-text",
+        },
+        {
+            title: "Gallery",
+            path: "/gallery",
+            className: "nav-text special",
+        },
+        {
+            title: "Leaderboard",
+            path: "/leaderboard",
+            className: "nav-text",
+        },
+        {
+            title: "Contact Us",
+            path: "/#contactus",
+            className: "nav-text",
+        },
+    ];
+
+    return (
+        <nav className="main-nav">
+            <Link to="/" className="logo">
+                <img src={ashvamedhaLogo} alt="ASHVAMEDHA" />
+            </Link>
+            <div
+                className={
+                    showMobileMenu ? "menu-link mobile-menu-link" : "menu-link"
+                }
+            >
+                <ul className="menu-items">
+                    {menuData.map(function (item, index) {
+                        return (
+                            <li key={index} className={item.className}>
+                                <Link
+                                    to={item.path}
+                                    onClick={() => setShowMobileMenu(false)}
+                                >
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="social-media">
+                {/* Hamburger menu start */}
+                <div className="hamburger-menu">
+                    <div onClick={() => setShowMobileMenu(!showMobileMenu)}>
+                        {showMobileMenu ? (
+                            <IoClose className="icon" />
+                        ) : (
+                            <IoMenu className="icon" />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ fd-vite-rewrite/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="Home">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Home;
